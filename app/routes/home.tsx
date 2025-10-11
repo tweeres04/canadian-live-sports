c10361bada38502e33ddf8ef97a7f34a0ff6a737
Tyler Weeres <tweeres04@gmail.com>
2025-10-11 16:13:54 -0700

    Rough list, loading TSN

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,79 @@
+import { useLoaderData } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Canadian Live Sports" },
+    { name: "description", content: "Find out what's on, quickly" },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+function isLive({
+  startTime,
+  endTime,
+}: {
+  startTime: string;
+  endTime: string;
+}) {
+  return new Date(endTime) > new Date() && new Date(startTime) < new Date();
+}
+
+type ScheduleItem = ReturnType<typeof tsnItemToScheduleItem>;
+
+function tsnItemToScheduleItem(tsnItem) {
+  return {
+    name: tsnItem.headlines.basic,
+    duration: tsnItem.duration,
+    startTime: tsnItem.startTime,
+    endTime: tsnItem.endTime,
+    channel: tsnItem.channelName,
+  };
+}
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const tsnSchedule = await fetch(
+    "https://www.tsn.ca/pf/api/v3/content/fetch/sports-schedule-custom"
+  ).then((data) => data.json());
+  const liveItems: ScheduleItem[] = tsnSchedule
+    .filter(isLive)
+    .map(tsnItemToScheduleItem)
+    .toSorted(
+      (a, b) =>
+        new Date(a.startTime).getTime() - new Date(b.startTime).getTime()
+    );
+  return liveItems;
+}
+
+export default function Home({
+  loaderData: tsnSchedule,
+}: Route.ComponentProps) {
+  return (
+    <div className="md:max-w-[900px] mx-auto space-y-5">
+      <h1 className="text-2xl">Canadian live sports</h1>
+      <ul className="space-y-3">
+        {tsnSchedule.map((scheduleItem) => (
+          <li>
+            <h2 className="text-xl">{scheduleItem.name}</h2>
+            <div className="flex">
+              {scheduleItem.channel} -{" "}
+              {new Intl.DateTimeFormat("en-CA", {
+                hour: "numeric",
+                minute: "2-digit",
+                hour12: true,
+              }).format(new Date(scheduleItem.startTime))}{" "}
+              -{" "}
+              {new Intl.DateTimeFormat("en-CA", {
+                hour: "numeric",
+                minute: "2-digit",
+                hour12: true,
+              }).format(new Date(scheduleItem.endTime))}
+            </div>
+          </li>
+        ))}
+        {/* {tsnSchedule.map((scheduleItem) => (
+          <pre>{JSON.stringify(scheduleItem, null, 4)}</pre>
+        ))} */}
+      </ul>
+    </div>
+  );
 }
